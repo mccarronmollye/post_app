3755a6ff2848d0ff312300b1bc2ce3052da0c918
Molly McCarron <mccarronmollye@gmail.com>
2019-04-19 16:29:30 -0400

    added seed data for posts

---
+++ src/data.js
@@ -0,0 +1,14 @@
+let posts = [
+  {
+    title: 'Doctor Strange',
+    content: 'Doggo ipsum blep fat boi fluffer what a nice floof you are doin me a concern, smol borking doggo with a long snoot for pats big ol pupper.'
+  },
+  {
+    title: 'Trolls',
+    content: 'Blop shoob floofs the neighborhood pupper many pats, length boy borkdrive.'
+  },
+  {
+    title: 'Jack Reacher: Never Go Back',
+    content: 'Yapper puggorino lotsa pats, very good spot. Extremely cuuuuuute length boy pupperino tungg ruff super chub.'
+  }
+]
